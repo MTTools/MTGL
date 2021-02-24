f10fae1903900a0a8607f3ac03ff620b30acc21a
Maciej Zaborowski <m4cias@protonmail.com>
2021-02-24 14:08:22 +0100

    ifndef for IMAGE_COMPRESSION_METHOD and IMAGE_DECOMPRESSION_BUFFER_SIZE

--- MTGL_image.h
+++ MTGL_image.h
@@ -7,10 +7,14 @@
 #define IMAGE_COMPRESSION_LZ77      1 // LZ77~ compression
 
 // used compression method
+#ifndef IMAGE_COMPRESSION_METHOD
 #define IMAGE_COMPRESSION_METHOD    IMAGE_COMPRESSION_LZ77
+#endif
 
 // image decompression buffer size
+#ifndef IMAGE_DECOMPRESSION_BUFFER_SIZE
 #define IMAGE_DECOMPRESSION_BUFFER_SIZE   (180 * 180)
+#endif
 
 #ifdef __cplusplus
 extern "C" {
